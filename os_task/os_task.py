2dcc7d0ded2a6a26c7e5ab94dcf26c880ba29c26
stotubalina <totubalina02@bk.ru>
2022-05-11 19:13:33 +0300

    добавлено задание с использованием модуля os

---
+++ os_task/os_task.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# -*- cosing: utf-8 -*-
+
+import os
+
+
+if __name__ == "__main__":
+    os.rename("dog.png", "cat.png")
+    os.remove("cat.png")
+    path = os.getcwd()
+    print(path)
+
+
+
